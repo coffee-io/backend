2a280d0819e2b49822e3cb5bb5d61aad3a313cbf
André Wagner <andre.nho@gmail.com>
2019-03-03 00:07:56 -0300

    .

--- terraform/api_hello.tf
+++ terraform/api_hello.tf
@@ -30,6 +30,13 @@ resource "aws_api_gateway_method_response" "hello_200" {
 	}
 }
 
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.coffee.id}"
+  resource_id = "${aws_api_gateway_resource.hello.id}"
+  http_method = "${aws_api_gateway_method.hello_get.http_method}"
+  status_code = "${aws_api_gateway_method_response.hello_200.status_code}"
+}
+
 data "aws_caller_identity" "current" {}
 
 resource "aws_lambda_permission" "hello_apigw_lambda" {
