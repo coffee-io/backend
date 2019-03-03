3e69fe9a240ebd0a31b12a15bd56f205891e5f23
André Wagner <andre.nho@gmail.com>
2019-03-03 00:21:17 -0300

    .

--- terraform/api.tf
+++ terraform/api.tf
@@ -1,3 +1,10 @@
 resource "aws_api_gateway_rest_api" "coffee" {
   name        = "coffee"
 }
+
+resource "aws_api_gateway_deployment" "coffee_deployment" {
+  depends_on = ["aws_api_gateway_integration.hello_integration"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.coffee.id}"
+  stage_name  = "prod"
+}
