0c10b76683a8d90243200b01a8ac6e0ebbfc29e5
André Wagner <andre.nho@gmail.com>
2019-03-03 12:24:07 -0300

    .

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM golang:1.12
 
 RUN apt-get update \
 	&& apt-get install -y awscli git zip \
-	&& go get github.com/aws/aws-lambda-go/lambda
+	&& go get github.com/aws/aws-lambda-go/lambda \
+	&& go get github.com/aws/aws-lambda-go/events
 
 #	&& go get github.com/aws/aws-sdk-go/aws \
 #	&& go get github.com/aws/aws-sdk-go/aws/session \
--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+    "encoding/json"
+    "github.com/aws/aws-lambda-go/events"
     "github.com/aws/aws-lambda-go/lambda"
+    "net/http"
 )
 
 type Request struct {}
@@ -10,12 +13,43 @@ type Response struct {
     Value string `json:"value"`
 }
 
-func Handler(request Request) (Response, error) {
-    return Response { Value: "world" }, nil
+func clientError(status int) (events.APIGatewayProxyResponse, error) {
+    return events.APIGatewayProxyResponse{
+        StatusCode: status,
+        Body:       http.StatusText(status),
+    }, nil
+}
+
+func serverError(err error) (events.APIGatewayProxyResponse, error) {
+    // errorLogger.Println(err.Error())
+
+    return events.APIGatewayProxyResponse{
+        StatusCode: http.StatusInternalServerError,
+        Body:       http.StatusText(http.StatusInternalServerError),
+    }, nil
+}
+
+func get(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+    js, err := json.Marshal(Response { Value: "world" })
+    if err != nil {
+        return serverError(err)
+    }
+    return events.APIGatewayProxyResponse{
+        StatusCode: http.StatusOK,
+        Body:       string(js),
+    }, nil
+}
+
+func router(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+    if req.HTTPMethod == "GET" {
+        return get(req)
+    } else {
+        return clientError(http.StatusMethodNotAllowed)
+    }
 }
 
 func main() {
-    lambda.Start(Handler);
+    lambda.Start(router);
 }
 
 // vim:st=4:sts=4:sw=4:expandtab
