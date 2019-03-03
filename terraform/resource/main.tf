6f5c28556ab01806682aab8b7aaf31323ebfbfad
André Wagner <andre.nho@gmail.com>
2019-03-03 14:04:24 -0300

    .

--- terraform/resource/main.tf
+++ terraform/resource/main.tf
@@ -74,7 +74,7 @@ module "cors" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${data.aws_api_gateway_rest_api.api.id}"
   resource_id   = "${aws_api_gateway_resource.resource.id}"
-  http_method   = "ANY"
+  http_method   = "GET"
 	authorization = "NONE"
 }
 
