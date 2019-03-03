ed107ddb9d2138f093d394860cd7b29c344ab1c5
André Wagner <andre.nho@gmail.com>
2019-03-03 07:33:44 -0300

    .

--- terraform/api_hello.tf
+++ terraform/api_hello.tf
@@ -49,3 +49,11 @@ resource "aws_lambda_permission" "hello_apigw_lambda" {
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.coffee.id}/*/*/*"
 }
+
+module "cors" {
+  source = "github.com/squidfunk/terraform-aws-api-gateway-enable-cors"
+  version = "0.2.0"
+
+  api_id          = "${aws_api_gateway_rest_api.coffee.id}"
+  api_resource_id = "${aws_api_gateway_resource.hello.id}"
+}
