845ee2832e9be5497cb146f176b894314f699ed6
André Wagner <andre.nho@gmail.com>
2019-03-03 00:28:05 -0300

    .

--- terraform/api.tf
+++ terraform/api.tf
@@ -28,7 +28,7 @@ resource "aws_api_gateway_domain_name" "coffee" {
 resource "aws_route53_record" "coffee-api" {
 	name    = "${aws_api_gateway_domain_name.coffee.domain_name}"
 	type	  = "A"
-	zone_id = "${aws_route53_zone.main.id}"
+	zone_id = "${data.aws_route53_zone.main.id}"
 	alias {
 		evaluate_target_health = true
 		name                   = "${aws_api_gateway_domain_name.coffee.cloudfront_domain_name}"
