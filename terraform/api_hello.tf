6d0d97c3038b300ffdb319fa5862649c81ee343f
André Wagner <andre.nho@gmail.com>
2019-03-02 23:55:41 -0300

    .

--- terraform/api_hello.tf
+++ terraform/api_hello.tf
@@ -15,8 +15,8 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.coffee.id}"
   resource_id             = "${aws_api_gateway_resource.hello.id}"
   http_method             = "${aws_api_gateway_method.hello_get.http_method}"
-  integration_http_method = "POST"
   type                    = "AWS"
+  integration_http_method = "POST"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.hello.arn}/invocations"
 }
 
