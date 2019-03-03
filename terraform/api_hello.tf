a0f175a3288a65aadbcc47dbabc175852d2dfa3e
André Wagner <andre.nho@gmail.com>
2019-03-03 09:42:02 -0300

    .

--- terraform/api_hello.tf
+++ terraform/api_hello.tf
@@ -4,40 +4,6 @@ resource "aws_api_gateway_resource" "hello" {
   path_part   = "hello"
 }
 
-resource "aws_api_gateway_method" "hello_get" {
-  rest_api_id   = "${aws_api_gateway_rest_api.coffee.id}"
-  resource_id   = "${aws_api_gateway_resource.hello.id}"
-  http_method   = "GET"
-	authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "hello_integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.coffee.id}"
-  resource_id             = "${aws_api_gateway_resource.hello.id}"
-  http_method             = "${aws_api_gateway_method.hello_get.http_method}"
-  type                    = "AWS"
-  integration_http_method = "POST"
-  uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.hello.arn}/invocations"
-}
-
-resource "aws_api_gateway_method_response" "hello_200" {
-  rest_api_id = "${aws_api_gateway_rest_api.coffee.id}"
-  resource_id = "${aws_api_gateway_resource.hello.id}"
-  http_method = "${aws_api_gateway_method.hello_get.http_method}"
-  status_code = "200"
-	response_models {
-		"application/json" = "Empty"
-	}
-}
-
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = "${aws_api_gateway_rest_api.coffee.id}"
-  resource_id = "${aws_api_gateway_resource.hello.id}"
-  http_method = "${aws_api_gateway_method.hello_get.http_method}"
-  status_code = "${aws_api_gateway_method_response.hello_200.status_code}"
-	depends_on = ["aws_api_gateway_integration.hello_integration"]
-}
-
 data "aws_caller_identity" "current" {}
 
 resource "aws_lambda_permission" "hello_apigw_lambda" {
