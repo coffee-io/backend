dfe1145805c412d3811e448713adef214f1730be
André Wagner <andre.nho@gmail.com>
2019-03-04 19:12:52 -0300

    .

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,7 @@ RUN apt-get update \
 	&& apt-get install -y awscli git zip \
 	&& go get github.com/aws/aws-lambda-go/lambda \
 	&& go get github.com/aws/aws-lambda-go/events
-
-#	&& go get github.com/aws/aws-sdk-go/aws \
-#	&& go get github.com/aws/aws-sdk-go/aws/session \
-#	&& go get github.com/aws/aws-sdk-go/service/dynamodb \
-#	&& go get github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute
+	&& go get github.com/aws/aws-sdk-go/aws \
+	&& go get github.com/aws/aws-sdk-go/aws/session \
+	&& go get github.com/aws/aws-sdk-go/service/dynamodb \
+	&& go get github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute
--- reset/reset.go
+++ reset/reset.go
@@ -1,10 +1,58 @@
 package main
 
 import (
-    "github.com/aws/aws-lambda-go/lambda"
+    "fmt"
+    "log"
+    "os"
+
+    _ "github.com/aws/aws-lambda-go/lambda"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
+var errorLog = log.New(os.Stderr, "ERROR ", log.Llongfile)
+
 func reset() (string, error) {
+    log.Println("Will now reset the database.")
+
+    // connect to dynamodb
+    session := session.Must(session.NewSession(&aws.Config{}))
+    svc := dynamodb.New(session)
+    log.Println("Connected to dynamodb.")
+
+    // Ingredients
+    type ingredient struct {
+        Name    string  `json:"name"`
+        Type    string  `json:"type"`
+        Color   string  `json:"color"`
+        Cost    float32 `json:"cost"`
+    }
+
+    type ingredients struct {
+        Ingredient_list []ingredient `json:"ingredients"`
+    }
+
+    my_ingredients := ingredients{
+        Ingredient_list: []ingredient{
+            ingredient{ Name: "Espresso", Type: "liquid", Color: "#141210", Cost: 4.0 },
+        },
+    }
+
+    av, err := dynamodbattribute.MarshalMap(my_ingredients)
+    input := &dynamodb.PutItemInput{
+        Item: av,
+        TableName: aws.String("CoffeeConfig"),
+    }
+    _, err = svc.PutItem(input)
+
+    if err != nil {
+        s := fmt.Sprintln(err.Error())
+        errorLog.Println(s)
+        return s, err
+    }
+
     return "Reset completed.", nil
 }
 
