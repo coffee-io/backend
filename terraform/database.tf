1c7855dee59632bdee5aa4d78bc5c6013f95bb8d
André Wagner <andre.nho@gmail.com>
2019-03-04 16:20:39 -0300

    .

---
+++ terraform/database.tf
@@ -0,0 +1,15 @@
+resource "aws_dynamodb_table" "coffee_data" {
+	name					= "CoffeeData"
+	billing_mode	= "PAY_PER_REQUEST"
+	hash_key      = "Key"
+
+	attribute {
+		name = "Key"
+		type = "S"
+	}
+
+	attribute {
+		name = "Data"
+		type = "S"
+	}
+}
