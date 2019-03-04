6d92100865241371e0a329859f2c9da08aebf4b7
André Wagner <andre.nho@gmail.com>
2019-03-04 14:52:56 -0300

    .

--- reset/reset.go
+++ reset/reset.go
@@ -3,12 +3,22 @@ package main
 import (
     "log"
     "os"
+
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/aws/aws-lambda-go/lambda"
 )
 
 var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
 
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+    log.Println(request);
+    return events.APIGatewayProxyResponse{
+        StatusCode: 200,
+    }, nil
+}
+
 func main() {
-    errorLogger.Println("Reset!");
+    lambda.Start(Handler)
 }
 
 // vim:st=4:sts=4:sw=4:expandtab
