84d32c90783fd0005f9f386a92f6806b8aa4902f
André Wagner <andre.nho@gmail.com>
2019-03-05 16:43:34 -0300

    .

--- terraform/api_ingredients.tf
+++ terraform/api_ingredients.tf
@@ -15,7 +15,7 @@ resource "aws_api_gateway_method" "method" {
 	authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_integration" "ingr_integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.coffee.id}"
   resource_id             = "${module.ingredients_resource.resource_id}"
   http_method             = "${aws_api_gateway_method.method.http_method}"
@@ -41,7 +41,7 @@ EOF
   }
 }
 
-resource "aws_api_gateway_method_response" "response_200" {
+resource "aws_api_gateway_method_response" "ingr_response_200" {
   rest_api_id = "${aws_api_gateway_rest_api.coffee.id}"
   resource_id = "${module.ingredients_resource.resource_id}"
   http_method = "${aws_api_gateway_method.method.http_method}"
@@ -54,4 +54,27 @@ resource "aws_api_gateway_method_response" "response_200" {
 	}
 }
 
+resource "aws_api_gateway_integration_response" "ingr_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.coffee.id}"
+  resource_id = "${module.ingredients_resource.resource_id}"
+  http_method = "${aws_api_gateway_method.method.http_method}"
+  status_code = "${aws_api_gateway_method_response.ingr_response_200.status_code}"
+	response_parameters = {
+		"method.response.header.Access-Control-Allow-Origin" = "'*'",
+	}
+  response_templates {
+    "application/xml" = <<EOF
+#set($inputRoot = $input.path('$'))
+[
+    #foreach($elem in $input.path('$.Items[0].configValue.L')) {
+        "name": "$elem.M.name.S",
+        "type": "$elem.M.type.S",
+        "color": "$elem.M.color.S",
+        "cost": $elem.M.cost.N
+    }#if($foreach.hasNext),#end
+#end
 
+]
+EOF
+	depends_on = ["aws_api_gateway_integration.ingr_integration"]
+}
