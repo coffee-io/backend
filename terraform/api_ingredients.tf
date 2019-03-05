b01769cc73b627f350748b7b8ee81bad1c0bb455
André Wagner <andre.nho@gmail.com>
2019-03-05 16:39:15 -0300

    .

--- terraform/api_ingredients.tf
+++ terraform/api_ingredients.tf
@@ -41,4 +41,17 @@ EOF
   }
 }
 
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = "${aws_api_gateway_rest_api.coffee.id}"
+  resource_id = "${module.ingredients_resource.resource_id}"
+  http_method = "${aws_api_gateway_method.method.http_method}"
+  status_code = "200"
+	response_models {
+		"application/json" = "Empty",
+	}
+	response_parameters = { 
+		"method.response.header.Access-Control-Allow-Origin" = true,
+	}
+}
+
 
