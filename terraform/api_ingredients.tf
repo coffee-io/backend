470bad732ccfa86b9b5c2bc94257ad21c0a1e1ff
André Wagner <andre.nho@gmail.com>
2019-03-05 16:21:45 -0300

    .

--- terraform/api_ingredients.tf
+++ terraform/api_ingredients.tf
@@ -7,3 +7,21 @@ module "ingredients_resource" {
 }
 
 # method
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.coffee.id}"
+  resource_id   = "${module.ingredients_resource.resource_id}"
+  http_method   = "GET"
+	authorization = "NONE"
+}
+
+/*
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.coffee.id}"
+  resource_id             = "${module.ingredients_resource.resource_id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
+}
+*/
