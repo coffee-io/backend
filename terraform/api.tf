0df437358dbca17ab3b1fdaee2cfe335aa580645
André Wagner <andre.nho@gmail.com>
2019-03-05 16:47:49 -0300

    .

--- terraform/api.tf
+++ terraform/api.tf
@@ -11,4 +11,8 @@ resource "aws_api_gateway_deployment" "coffee_deployment" {
 
   rest_api_id = "${aws_api_gateway_rest_api.coffee.id}"
   stage_name  = "prod"
+
+	triggers {
+		build_number = "${timestamp()}" 
+	}
 }
