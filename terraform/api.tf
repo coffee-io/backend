2a82b87160ca322b09ab84d6996e2bb29f9a3666
André Wagner <andre.nho@gmail.com>
2019-03-06 09:15:04 -0300

    .

--- terraform/api.tf
+++ terraform/api.tf
@@ -12,13 +12,12 @@ resource "aws_api_gateway_deployment" "coffee_deployment" {
   rest_api_id = "${aws_api_gateway_rest_api.coffee.id}"
   stage_name  = "prod"
 
-	/*
 	variables {
 		deployed_at = "${timestamp()}"
 	}
-	*/
 }
 
+/*
 resource "null_resource" "redeploy_api" {
 	depends_on = ["aws_api_gateway_deployment.coffee_deployment"]
 	provisioner "local-exec" {
@@ -26,5 +25,16 @@ resource "null_resource" "redeploy_api" {
 	}
 	triggers {
 		build_number = "${timestamp()}" 
+	}
+}
+*/
+
+resource "aws_api_gateway_base_path_mapping" "coffee" {
+  api_id      = "${aws_api_gateway_rest_api.coffee.id}"
+  stage_name  = "${aws_api_gateway_deployment.coffee_deployment.stage_name}"
+  domain_name = "${aws_api_gateway_domain_name.coffee.domain_name}"
+
+	variables {
+		deployed_at = "${timestamp()}"
 	}
 }
