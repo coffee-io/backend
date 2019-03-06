7ae1ea96baa33a93247ecda53b86de6ebbef0745
André Wagner <andre.nho@gmail.com>
2019-03-06 10:07:15 -0300

    .

--- reset/reset.py
+++ reset/reset.py
@@ -6,8 +6,19 @@ def update_ingredients(config):
     response = config.put_item(Item={
         'configKey': 'ingredients',
         'configValue': [
-            { 'name':'Espresso', 'type':'liquid', 'color':'#141210', 'cost':Decimal(4.0) },
-            { 'name':'Brewed', 'type':'liquid', 'color':'#242220', 'cost':Decimal(3.0) },
+            { 'name':'Espresso',        'type':'coffee', 'color':'#000000', 'cost':Decimal(4.0) },
+            { 'name':'Brewed (strong)', 'type':'coffee', 'color':'#610B0B', 'cost':Decimal(3.0) },
+            { 'name':'Brewed (weak)',   'type':'coffee', 'color':'#8A4B08', 'cost':Decimal(3.0) },
+            { 'name':'Water',           'type':'liquid', 'color':'#0080FF', 'cost':Decimal(0.0) },
+            { 'name':'Cream',           'type':'liquid', 'color':'#F5F6CE', 'cost':Decimal(4.0) },
+            { 'name':'Milk',            'type':'liquid', 'color':'#FAFAFA', 'cost':Decimal(2.0) },
+            { 'name':'Whipped milk',    'type':'liquid', 'color':'#F2F2F2', 'cost':Decimal(3.0) },
+            { 'name':'Chocolate',       'type':'liquid', 'color':'#8A4B08', 'cost':Decimal(5.0) },
+            { 'name':'Whisky',          'type':'liquid', 'color':'#FFBF00', 'cost':Decimal(12.0) },
+            { 'name':'Sugar',           'type':'added',  'color':'#FAFAFA', 'cost':Decimal(0.0) },
+            { 'name':'Cinnamon',        'type':'added',  'color':'#5F4C0B', 'cost':Decimal(0.0) },
+            { 'name':'Nutmeg',          'type':'added',  'color':'#DBA901', 'cost':Decimal(0.0) },
+            { 'name':'Sucralose',       'type':'added',  'color':'#E0E6F8', 'cost':Decimal(0.0) },
         ]
     })
     print('Ingredients updated.')
