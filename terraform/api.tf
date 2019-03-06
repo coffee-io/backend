a8f0fd76e31c4f86fa8c6db662d302ef3ebb721c
André Wagner <andre.nho@gmail.com>
2019-03-06 09:16:12 -0300

    .

--- terraform/api.tf
+++ terraform/api.tf
@@ -33,8 +33,4 @@ resource "aws_api_gateway_base_path_mapping" "coffee" {
   api_id      = "${aws_api_gateway_rest_api.coffee.id}"
   stage_name  = "${aws_api_gateway_deployment.coffee_deployment.stage_name}"
   domain_name = "${aws_api_gateway_domain_name.coffee.domain_name}"
-
-	variables {
-		deployed_at = "${timestamp()}"
-	}
 }
