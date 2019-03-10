45008500ffe4323a41fff692836425484820c2b1
André Wagner <andre.nho@gmail.com>
2019-03-10 18:18:29 -0300

    .

---
+++ terraform/api_cart.tf
@@ -0,0 +1,33 @@
+# lambda
+
+module "lambda_recalculate" {
+	source          = "./lambda"
+	name            = "recalculate"
+	lambda_role_arn = "${aws_iam_role.iam_for_lambda.arn}"
+}
+
+# resource
+
+module "cart_resource" {
+  source          = "./resource"
+  rest_api_name   = "${aws_api_gateway_rest_api.coffee.name}"
+  parent_id       = "${aws_api_gateway_rest_api.coffee.root_resource_id}"
+  name            = "cart"
+}
+
+module "cart_calculator_resource" {
+  source          = "./resource"
+  rest_api_name   = "${aws_api_gateway_rest_api.coffee.name}"
+  parent_id       = "${module.cart_resource.resource_id}"
+  name            = "global"
+}
+
+# methods
+
+module "cart_calulator_get" {
+	source					= "./api_method_lambda"
+	rest_api_name   = "${aws_api_gateway_rest_api.coffee.name}"
+	resource_id     = "${module.cart_calculator.resource_id}"
+	http_method     = "GET"
+	lambda_arn      = "${module.lambda_recalculate.lambda_arn}"
+}
