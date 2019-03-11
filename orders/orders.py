8a210ebdbc2a3296e4bdccbf79c81999cc04963a
André Wagner <andre.nho@gmail.com>
2019-03-11 06:32:11 -0300

    .

--- orders/orders.py
+++ orders/orders.py
@@ -1,17 +1,41 @@
 import boto3
+import decimal
 import json
+import requests
 import traceback
+import sys
+from datetime import datetime
 
-def verify_cart(cart):
-    pass  # TODO - go the verification service to check for values
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+def verify_cart(cart_json):
+    url = "https://coffee-api.gamesmith.co.uk/cart/calculator"
+    response = requests.put(url, data=cart_json)
+    return data
 
 def save_order(cart):
-    pass  # TODO - save order on dynamodb
+    dynamodb = boto3.resource('dynamodb')
+    recipes = dynamodb.Table('CoffeeOrders')
+    recipes.put_item(Item={ 
+        **{
+            'userEmail': cart['deliveryAddress']['email'],
+            'orderDate': datetime.now().isoformat(),
+        }, 
+        **cart
+    })
 
-def main_handler(cart, context):
+def main_handler(event, context):
     try:
+        cart_json = event['body']
         cart = json.loads(event['body'])
-        verify_cart(cart)
+        cart = verify_cart(cart_json)
         save_order(cart)
         return {
             'statusCode': 201, # created
@@ -21,5 +45,11 @@ def main_handler(cart, context):
             'statusCode': 400,
             'body': ''.join(traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__))
         }
+
+if __name__ == '__main__':
+    cart = {}
+    with open("test.json") as json_file:
+        data = json.load(json_file, parse_float = decimal.Decimal)
+    print(main_handler({ 'body': json.dumps(data, cls=DecimalEncoder, ensure_ascii=False) }, None))
 
 # vim:st=4:sts=4:sw=4:expandtab
