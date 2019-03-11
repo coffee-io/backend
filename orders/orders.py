11f8df3c9afc3ee6437fa756348d67acb7471905
andrenho <xmate156>
2019-03-11 07:24:20 -0300

    .

--- orders/orders.py
+++ orders/orders.py
@@ -18,6 +18,7 @@ class DecimalEncoder(json.JSONEncoder):
 def verify_cart(cart_json):
     url = "https://coffee-api.gamesmith.co.uk/cart/calculator"
     response = requests.put(url, data=cart_json)
+    print(response)
     return json.loads(response)
 
 def save_order(cart):
