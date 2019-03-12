46aac05cb53705107fee248baf020ab42e0b9d9f
andrenho <xmate156>
2019-03-12 09:35:19 -0300

    .

--- terraform/api_method_lambda/main.tf
+++ terraform/api_method_lambda/main.tf
@@ -49,6 +49,10 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
 
 data "aws_caller_identity" "current" {}
 
+data "aws_api_gateway_resource" "resource" {
+  id = "${var.resource_id}"
+}
+
 resource "aws_lambda_permission" "resource_apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -56,6 +60,6 @@ resource "aws_lambda_permission" "resource_apigw_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${data.aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}/${aws_api_gateway_method.method.path}"
+  source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${data.aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}/${aws_api_gateway_resource.resource.path}"
 }
 
